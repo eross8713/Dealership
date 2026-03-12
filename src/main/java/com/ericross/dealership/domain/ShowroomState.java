0f9ee59fee029e82972e759461765b241b011798
Eric Ross <eric.ross1@va.gov>
2026-03-12 09:18:11 -0400

    housekeeping

--- src/main/java/com/ericross/dealership/domain/ShowroomState.java
+++
@@ -1,9 +0,0 @@
-package com.ericross.dealership.domain;
-
-public class ShowroomState {
-    private final HondaShowroom hondaShowroom = new HondaShowroom();
-    private final ToyotaShowroom toyotaShowroom = new ToyotaShowroom();
-
-    public HondaShowroom getHondaShowroom() { return hondaShowroom; }
-    public ToyotaShowroom getToyotaShowroom() { return toyotaShowroom; }
-}
--- src/main/java/com/ericross/dealership/service/ModelCommand.java
+++
@@ -1,9 +0,0 @@
-package com.ericross.dealership.service;
-
-import com.ericross.dealership.dtos.VehicleCandidateDto;
-import com.ericross.dealership.domain.ShowroomState;
-
-@FunctionalInterface
-public interface ModelCommand {
-    void apply(VehicleCandidateDto req, ShowroomState state);
-}
